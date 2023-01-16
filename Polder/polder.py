4cb2635556b880804124fc27a1a4b3e6227ca859
yemoski <yemisiogungbemi89@gmail.com>
2023-01-16 12:35:09 -0800

    'checks'

--- Polder/polder.py
+++ Polder/polder.py
@@ -1,17 +1,11 @@
 import requests
 import json
 
+
 def get_total_results():
-	link = "https://search.polder.info/api/count"
-	response = requests.get(link)
+    link = "https://search.polder.info/api/count"
+    response = requests.get(link)
 
-	# Convert JSON data to a python object
-	data = json.loads(response.text)
-	return data
-
-
-
-
-
-
-
+    # Convert JSON data to a python object
+    data = json.loads(response.text)
+    return data
